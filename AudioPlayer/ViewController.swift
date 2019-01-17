33298a0145ed041b90b3f00c3d0a4352d096a07a
Andrew Borisov <shadowreal85@gmail.com>
2019-01-17 18:41:16 +0300

    Initial Commit

---
+++ AudioPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Andrew Borisov on 1/17/19.
+//  Copyright © 2019 Andrew Borisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
